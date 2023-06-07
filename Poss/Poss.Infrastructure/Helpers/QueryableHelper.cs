f5037db98aa784afadcd779a19e7df18d1266717
Hansel Cabrera <hcabrera@segurnet.com.do>
2023-06-07 17:26:37 -0400

    Helpers

---
+++ Poss/Poss.Infrastructure/Helpers/QueryableHelper.cs
@@ -0,0 +1,15 @@
+﻿using Poss.Infrastructure.Commons.Bases;
+namespace Poss.Infrastructure.Helpers
+{
+    public static class QueryableHelper
+    {
+        //public static IQueryable<TEntity> Paginate(this IQueryable<T> queryable, BasePaginationRequest request) 
+        //{
+        //    return queryable.Skip((request.NumPage - 1) * request.Records).Take(request.Records);
+
+        
+        
+        //}
+
+    }
+}
